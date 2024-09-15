d0ea094653bcf2e6f297c5ca106ed281113ec563
cmotta000 <luismiguelcmt@gmail.com>
2024-09-15 13:38:01 -0300

    Update SmartTv.java

--- SmartTv.java
+++ SmartTv.java
@@ -1,41 +1,65 @@
 public class SmartTv {
-    boolean ligada=false;
-    int canal=1;
-    int volume=28;
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 28;
+    boolean wifiConectado= true;
+    String apps = "Escolha seu canal";
+    String app1 = "YOUTUBE";
+    String app2 = "NETFLIX";
+    String app3 = "AMAZON";
+    String app4 = "SPOTIFY";
+    double resolução = 1.5;
+    
 
-    public void ligar(){
-    ligada=true;
+    public void ligar() {
+        ligada = true;
     }
-    public void desligar(){
-    ligada=false;  
+
+    public void desligar() {
+        ligada = false;
     }
-    public void aumentarVolume(){
+
+    public void aumentarVolume() {
         volume++;
 
     }
 
-    public void diminuirVolume(){
+    public void diminuirVolume() {
         volume--;
 
     }
 
-    
-    public void mudarCanal(int novoCanal){
-        canal= novoCanal;
-
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
 
     }
 
-    public void aumentarCanal(){
+    public void aumentarCanal() {
         canal++;
-  
 
     }
 
-    
-    public void diminuirCanal(){
+    public void diminuirCanal() {
         canal--;
+    }
+
+    public void escolherApp() {
+        apps = app1;
+
+    }
+
+    public void aumentarResolução() {
+        resolução++;
+    }
+
+    public void diminuirResolução() {
+        resolução--;
+    }
+
+    public void conectarWifi(){
 
     }
 
 }
+
+
